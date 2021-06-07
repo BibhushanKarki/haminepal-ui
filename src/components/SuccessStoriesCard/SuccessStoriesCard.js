6929023ad3fc0b8c765e1e077566834efd95331d
Bibhushan <bibhushan.karki11@gmail.com>
2021-06-07 18:08:10 +0545

    image size of success stories made responsive

--- src/components/SuccessStoriesCard/SuccessStoriesCard.js
+++ src/components/SuccessStoriesCard/SuccessStoriesCard.js
@@ -6,17 +6,17 @@ import Zoom from "react-reveal/Zoom";
 const SuccessStories = () => {
   return (
     <div className="behindTheNameCardContainer my-4">
-      <div className="row justify-content-center ">
-        <h2
+      <div className="row justify-content-center mx-2">
+        <h1
           className="fw-bold my-3 text-center"
-          style={{ color: "#9F1718", marginLeft: "2rem" }}
+          style={{ color: "#9F1718", marginLeft: "2rem",textShadow: '5px 5px 5px grey'}}
         >
           Success Stories
-        </h2>
+        </h1>
         {data.map((data, key) => {
           return (
             <div
-              className="col-md-10 mb-3 d-flex justify-content-center text-center"
+              className="col-lg-10 col-md-10 mb-3 d-flex justify-content-center text-center"
               key={key}
             >
               <Zoom>
@@ -28,7 +28,7 @@ const SuccessStories = () => {
                     >
                       {data.storyTitle}
                     </h4>
-                    <img src={data.img} alt="storyimg" />
+                    <img src={data.img} alt="storyimg" style={{width:'100%'}}/>
                     <p className="card-text text-left my-4">
                       {data.storyDetail}
                     </p>
