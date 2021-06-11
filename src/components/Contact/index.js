4ac7173f00ec699d9a885ae0f6ac5b9b8a379b3f
Sushma Chaudhary <susmachaudhary@gmail.com>
2021-06-11 08:00:11 +0545

    updated contact pages

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,108 +2,108 @@ import React from "react";
 
 const Conatct = () => {
   return (
-    <section id="contact" className="contact">
-      <div className="container" data-aos="fade-up">
-        <div className=" section-title text-center">
-          <h2>Contact</h2>
-          <p>Contact Us</p>
-        </div>
+    <div>
+      <section id="contact" className="contact">
+        <div className="container" data-aos="fade-up">
+          <div className=" section-title text-center">
+            <h2>Contact</h2>
+            <p>Contact Us</p>
+          </div>
 
-        <div className="row">
-          <div className="col-lg-6">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="info-box">
-                  <i className="bx bx-map"></i>
-                  <h3>Our Address</h3>
-                  <p>Ranibari-27, Samakhusi Kathmandu</p>
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="info-box">
+                    <i className="bx bx-map"></i>
+                    <h3>Our Address</h3>
+                    <p>Ranibari-27, Samakhusi Kathmandu</p>
+                  </div>
                 </div>
-              </div>
-              <div className="col-md-6">
-                <div className="info-box mt-4">
-                  <i className="bx bx-envelope"></i>
-                  <h3>Email Us</h3>
-                  <p>
-                    info@haminepal.org
-                    {/* <br />
+                <div className="col-md-6">
+                  <div className="info-box mt-4">
+                    <i className="bx bx-envelope"></i>
+                    <h3>Email Us</h3>
+                    <p>
+                      info@haminepal.org
+                      {/* <br />
                     info@haminepal.org */}
-                  </p>
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="col-md-6">
-                <div className="info-box mt-4">
-                  <i className="bx bx-phone-call"></i>
-                  <h3>Call Us</h3>
-                  <p>
-                    +977 9808543667, +977 9845628191
-                  </p>
+                <div className="col-md-6">
+                  <div className="info-box mt-4">
+                    <i className="bx bx-phone-call"></i>
+                    <h3>Call Us</h3>
+                    <p>+977 9808543667, +977 9845628191</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="col-lg-6 mt-4 mt-lg-0">
-            <form
-              action="forms/contact.php"
-              role="form"
-              className="php-email-form"
-            >
-              <div className="row">
-                <div className="col-md-6 form-group">
+            <div className="col-lg-6 mt-4 mt-lg-0">
+              <form
+                action="forms/contact.php"
+                role="form"
+                className="php-email-form"
+              >
+                <div className="row">
+                  <div className="col-md-6 form-group">
+                    <input
+                      type="text"
+                      name="name"
+                      className="form-control"
+                      id="name"
+                      placeholder="Your Name"
+                      required
+                    />
+                  </div>
+                  <div className="col-md-6 form-group mt-3 mt-md-0">
+                    <input
+                      type="email"
+                      className="form-control"
+                      name="email"
+                      id="email"
+                      placeholder="Your Email"
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="form-group mt-3">
                   <input
                     type="text"
-                    name="name"
                     className="form-control"
-                    id="name"
-                    placeholder="Your Name"
+                    name="subject"
+                    id="subject"
+                    placeholder="Subject"
                     required
                   />
                 </div>
-                <div className="col-md-6 form-group mt-3 mt-md-0">
-                  <input
-                    type="email"
+                <div className="form-group mt-3">
+                  <textarea
                     className="form-control"
-                    name="email"
-                    id="email"
-                    placeholder="Your Email"
+                    name="message"
+                    rows="5"
+                    placeholder="Message"
                     required
-                  />
+                  ></textarea>
                 </div>
-              </div>
-              <div className="form-group mt-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="subject"
-                  id="subject"
-                  placeholder="Subject"
-                  required
-                />
-              </div>
-              <div className="form-group mt-3">
-                <textarea
-                  className="form-control"
-                  name="message"
-                  rows="5"
-                  placeholder="Message"
-                  required
-                ></textarea>
-              </div>
-              <div className="my-3">
-                <div className="loading">Loading</div>
-                <div className="error-message"></div>
-                <div className="sent-message">
-                  Your message has been sent. Thank you!
+                <div className="my-3">
+                  <div className="loading">Loading</div>
+                  <div className="error-message"></div>
+                  <div className="sent-message">
+                    Your message has been sent. Thank you!
+                  </div>
                 </div>
-              </div>
-              <div className="text-center">
-                <button type="submit">Send Message</button>
-              </div>
-            </form>
+                <div className="text-center">
+                  <button type="submit">Send Message</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
