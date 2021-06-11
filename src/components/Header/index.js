d5cfb31a03fec661c5722ed3a020812b30b7d3ea
Bibhushan Karki <bibhushan.karki11@gmail.com>
2021-06-11 13:40:59 +0545

    navbar changed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,28 +3,31 @@ import { Link } from "react-router-dom";
 import { ProjectSectionData } from "../ProjectPageSection/ProjectSectionData";
 
 const Header = () => {
-  const [isActive, setActive] = useState("false");
-  const [isIconActive, setIconActive] = useState("false");
-  // const [isDropdownActive, setDropdownActive] = useState("false");
-  const [isSmallDropdownActive, setSmallDropdownActive] = useState("false");
-  const handleToggle = () => {
-    setActive(!isActive);
-    setIconActive(!isIconActive);
-  };
-  const handleIconToggle = () => {
-    setIconActive(!isIconActive);
-    setActive(!isActive);
-  };
-  // const handleDropdownToggle = () => {
-  //         setDropdownActive(!isDropdownActive);
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+  // const [isActive, setActive] = useState("false");
+  // const [isIconActive, setIconActive] = useState("false");
+  // // const [isDropdownActive, setDropdownActive] = useState("false");
+  // const [isSmallDropdownActive, setSmallDropdownActive] = useState("false");
+  // const handleToggle = () => {
+  //   setActive(!isActive);
+  //   setIconActive(!isIconActive);
   // };
-  const handleSmallDropdownToggle = () => {
-    setSmallDropdownActive(!isSmallDropdownActive);
-  };
+  // const handleIconToggle = () => {
+  //   setIconActive(!isIconActive);
+  //   setActive(!isActive);
+  // };
+  // // const handleDropdownToggle = () => {
+  // //         setDropdownActive(!isDropdownActive);
+  // // };
+  // const handleSmallDropdownToggle = () => {
+  //   setSmallDropdownActive(!isSmallDropdownActive);
+  // };
   return (
     <header id="header" className="fixed-top header-scrolled">
-      <div className="container d-flex align-items-center justify-content-between">
-        {/* <h1 className="logo"><Link to="/">Hami Nepal</Link></h1> */}
+      {/* <div className="container d-flex align-items-center justify-content-between">
+        
         <Link to="/" className="logo">
           <img src="/assets/img/logo-only.png" alt="" className="img-fluid" />
         </Link>
@@ -35,17 +38,17 @@ const Header = () => {
         >
           <ul>
             <li>
-              <Link className="nav-link scrollto active" to="/">
+              <Link className="nav-link active" to="/">
                 Home
               </Link>
             </li>
             <li>
-              <Link className="nav-link scrollto" to="/volunteer-signup">
+              <Link className="nav-link" to="/volunteer-signup">
                 Be Volunteer
               </Link>
             </li>
             <li className="dropdown">
-              <Link href="#">
+              <Link to="/">
                 <span>Cause</span>{" "}
                 <i
                   className="bi bi-chevron-down"
@@ -99,7 +102,7 @@ const Header = () => {
             </li>
 
             <li className="dropdown">
-              <Link href="#">
+              <Link to="/">
                 <span>Donate</span>{" "}
                 <i
                   className="bi bi-chevron-down"
@@ -164,7 +167,187 @@ const Header = () => {
             onClick={(handleToggle, handleIconToggle)}
           ></i>
         </nav>
-      </div>
+      </div> */}
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid d-flex justify-content-between">
+          <Link to="/" className="logo">
+            <img src="/assets/img/logo-only.png" alt="" className="img-fluid" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+            id="navbarNavDropdown"
+          >
+            <ul
+              className="navbar-nav"
+              style={{ display: !isNavCollapsed ? "none" : "flex" }}
+            >
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/volunteer-signup">
+                  Be Volunteer
+                </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to="/"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Cause
+                </Link>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <li>
+                    <Link className="dropdown-item" to="/covid">
+                      COVID
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/health">
+                      Health
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/housing">
+                      Housing
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/clothing">
+                      Clothing
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/food">
+                      Food
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/education">
+                      Education
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to="/"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Projects
+                </Link>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  {ProjectSectionData.map((keyBundle) => (
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to={`/events/${keyBundle.key}`}
+                        key={keyBundle.key}
+                      >
+                        {keyBundle.displayName}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to="/"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Donate
+                </Link>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <li>
+                    <Link className="dropdown-item" to="/donate-covid">
+                      COVID
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/cause">
+                      Cause
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/volunteer">
+                      Volunteer
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/donate-administration">
+                      Administration
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/tranparency">
+                  Transparency
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/aboutus">
+                  About Us
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/community">
+                  Our Community
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sign-in">
+                  Sign In
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link getStarted" to="/sign-up">
+                  Join Now
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
