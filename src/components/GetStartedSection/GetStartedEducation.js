b3093b991d0835a6e735bf454911445da351e398
Sushma Chaudhary <susmachaudhary@gmail.com>
2021-06-11 13:39:13 +0545

    updated cause page

--- src/components/GetStartedSection/GetStartedEducation.js
+++ src/components/GetStartedSection/GetStartedEducation.js
@@ -10,7 +10,7 @@ const GetStartedEducation = () => {
       <div className="row d-flex align-items-center m-4 main-content">
         <Fade left>
           <div className="col-lg-6 get-started-title">
-            <h1>{data.education.title}</h1>
+            <h1 className=" text-start">{data.education.title}</h1>
             <h4 className="my-4" style={{ color: "gray" }}>
               {data.education.punchline}
             </h4>
