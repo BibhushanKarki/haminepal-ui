1f9224d83288b2e325f97b181c0f5438565ed061
Sushma Chaudhary <susmachaudhary@gmail.com>
2021-06-13 10:47:00 +0545

    linked how to be a volunteer page

--- src/Routes.js
+++ src/Routes.js
@@ -35,57 +35,63 @@ import OurObjective from "./pages/OurObjective";
 import DonationErrorPage from "./pages/DonationErrorPage";
 import DonationSuccessPage from "./pages/DonationSuccessPage";
 
-import ProtectedRoute from './utils/ProtectedRoute';
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 const Routes = () => {
-    const donationIsSet = JSON.parse(localStorage.getItem('donation')) ? true : false;
-    console.log(donationIsSet, 'donation is set', JSON.parse(localStorage.getItem('donation')))
+  const donationIsSet = JSON.parse(localStorage.getItem("donation"))
+    ? true
+    : false;
+  console.log(
+    donationIsSet,
+    "donation is set",
+    JSON.parse(localStorage.getItem("donation"))
+  );
 
-    return (
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/aboutus" component={AboutUs} />
-        <Route path="/events/:key" component={Projects} />
-        <Route path="/event/:key/:slug" component={ProjectExtended} />
-        <Route path="/privacy-policy" component={PrivacyPolicy} />
-        <Route path="/volunteer" component={Volunteer} />
-        <Route path="/be-volunteer" component={BeVolunteer} />
-        <Route path="/volunteer-signup" component={VolunteerSignUp} />
-        <Route path="/volunteer-profile" component={VolunteerProfile} />
-        <Route path="/star-rating" component={StarRating} />
-        <Route path="/community" component={OurCommunity} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/fundraisers" component={Fundraiser} />
-        <Route path="/transparencydetail" component={TransparencyDetail} />
-        <Route path="/sign-in" component={SignIn} />
-        <Route path="/sign-up" component={SignUp} />
-        <Route path="/covid" component={Covid} />
-        <Route path="/food" component={Food} />
-        <Route path="/health" component={Health} />
-        <Route path="/education" component={Education} />
-        <Route path="/clothing" component={Clothing} />
-        <Route path="/housing" component={Housing} />
-        <Route path="/donate-covid" component={Donate_Covid} />
-        <Route path="/cause" component={Cause} />
-        <Route path="/transparency" component={Transparency} />
-        <Route path="/donate-administration" component={DonateAdministration} />
-        <Route path="/howitworks" component={HowItWorks} />
-        <Route path="/whyhaminepal" component={WhyHamiNepal} />
-        <Route path="/success-stories" component={SuccessStories} />
-        <Route path="/our-objectives" component={OurObjective} />
-        <ProtectedRoute
-          path="/donation/success"
-          component={DonationSuccessPage}
-          condition={donationIsSet}
-        />
-        <ProtectedRoute
-          path="/donation/error"
-          component={DonationErrorPage}
-          condition={donationIsSet}
-        />
-        <Route path="/faq" component={Faq} />
-      </Switch>
-    );
+  return (
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/aboutus" component={AboutUs} />
+      <Route path="/events/:key" component={Projects} />
+      <Route path="/event/:key/:slug" component={ProjectExtended} />
+      <Route path="/privacy-policy" component={PrivacyPolicy} />
+      <Route path="/volunteer" component={Volunteer} />
+      <Route path="/how-to-be-a-volunteer" component={BeVolunteer} />
+      <Route path="/volunteer-signup" component={VolunteerSignUp} />
+      <Route path="/volunteer-profile" component={VolunteerProfile} />
+      <Route path="/star-rating" component={StarRating} />
+      <Route path="/community" component={OurCommunity} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/fundraisers" component={Fundraiser} />
+      <Route path="/transparencydetail" component={TransparencyDetail} />
+      <Route path="/sign-in" component={SignIn} />
+      <Route path="/sign-up" component={SignUp} />
+      <Route path="/covid" component={Covid} />
+      <Route path="/food" component={Food} />
+      <Route path="/health" component={Health} />
+      <Route path="/education" component={Education} />
+      <Route path="/clothing" component={Clothing} />
+      <Route path="/housing" component={Housing} />
+      <Route path="/donate-covid" component={Donate_Covid} />
+      <Route path="/cause" component={Cause} />
+      <Route path="/transparency" component={Transparency} />
+      <Route path="/donate-administration" component={DonateAdministration} />
+      <Route path="/howitworks" component={HowItWorks} />
+      <Route path="/whyhaminepal" component={WhyHamiNepal} />
+      <Route path="/success-stories" component={SuccessStories} />
+      <Route path="/our-objectives" component={OurObjective} />
+      <ProtectedRoute
+        path="/donation/success"
+        component={DonationSuccessPage}
+        condition={donationIsSet}
+      />
+      <ProtectedRoute
+        path="/donation/error"
+        component={DonationErrorPage}
+        condition={donationIsSet}
+      />
+      <Route path="/faq" component={Faq} />
+    </Switch>
+  );
 };
 
 export default Routes;
