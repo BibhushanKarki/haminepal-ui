9f82792b9393a1fd007a4a7cfd2ef83a8e9508bb
Bibhushan Karki <bibhushan.karki11@gmail.com>
2021-06-13 12:18:46 +0545

    navbar bug fixed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -185,6 +185,7 @@ const Header = () => {
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
+            onClick={handleNavCollapse}
             data-bs-target="#navbarNavDropdown"
             aria-controls="navbarNavDropdown"
             aria-expanded="false"
@@ -198,6 +199,7 @@ const Header = () => {
           >
             <ul
               className="navbar-nav w-100 justify-content-end"
+              onClick={handleNavCollapse}
               style={{ display: !isNavCollapsed ? "none" : "flex" }}
             >
               <li className="nav-item">
